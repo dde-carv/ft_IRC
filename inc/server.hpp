dc4e50a4229f2c2d81eef985266c384bc204b2b3
Ricardo Manuel Ribeiro Alcobia <ricmanue@c1r10s9.42lisboa.com>
2025-12-05 10:04:53 +0000

    first files

---
+++ inc/server.hpp
@@ -0,0 +1,22 @@
+
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <poll.h>
+#include <csignal>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <ctime>
+
+
---
+++ src/server.c
@@ -0,0 +1,14 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+
+
+int main()
+{
+	socket();
+
+	bind();
+
+	send();
+
+	epoll();
+}
